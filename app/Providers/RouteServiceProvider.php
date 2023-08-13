d4cbfc8df7af75633d956069ca0cf353d2612105
sashokrist <alexander.krist@gmail.com>
2023-08-13 16:38:28 +0300

    fix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/desks';
+    public const HOME = '/api/desks';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
